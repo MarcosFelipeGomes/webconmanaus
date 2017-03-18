7c36318858f1b3b22f99f9d5c74b5be8beee5139
Marcos Felipe Gomes <marcos.felipe.gomes@hotmail.com>
2017-03-18 16:52:25 -0400

    Atualizando site, incluindo letra personalizada e logos

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'marcos.felipe.gomes@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'suporte.ti@webconmanaus.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "E-Mail de Contato da Webcon:  $name";
 $email_body = "Você recebeu uma nova mensagem do seu E-Mail de Contato da Webcon.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
-$headers = "From: marcos.felipe.gomes@hotmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: suporte.ti@webconmanaus.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
